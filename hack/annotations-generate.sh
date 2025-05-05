8a96783c262bff1fbea314556903a0be3ab68741
Mert Bicak <mbicak@redhat.com>
2025-05-05 16:34:40 +0200

    feat: add scripts to generate bundle metadata and docs from opa inspect annotations

---
+++ hack/annotations-generate.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+OUT_DIR=policy_annotations
+OPA=./opa
+
+mkdir $OUT_DIR
+
+shopt -s nullglob
+
+for dir in ./policy/*/ ; do
+  if [[ -d "$dir" ]]; then
+    # Extract the directory name for the output file
+    dirname=$(basename "$dir")
+    outfile="$OUT_DIR/inspection_${dirname}.json"
+
+    echo "Inspecting annotations for: $dir and saving to $outfile"
+    $OPA inspect -f json -a "$dir" > "$outfile"
+    echo "---"
+  fi
+done
+
+shopt -u nullglob
